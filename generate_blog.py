5893fa395912c65948b520660b8fd7520fe3266a
Stefan <ssttefancarter@gmail.com>
2025-09-17 16:35:58 +0100

    Updated logo

--- generate_blog.py
+++ generate_blog.py
@@ -36,7 +36,7 @@ INDEX_HEADER = """<!DOCTYPE html>
 <body>
  <nav class="navbar navbar-expand-lg">
     <div class="container">
-      <a class="navbar-brand" href="../">N</a>
+      <a class="navbar-brand" href="../"><img src="../images/logo.png" alt="Logo" height="75px"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -112,7 +112,7 @@ POST_TEMPLATE = """<!DOCTYPE html>
 <body>
  <nav class="navbar navbar-expand-lg">
     <div class="container">
-      <a class="navbar-brand" href="../">N</a>
+      <a class="navbar-brand" href="../"><img src="../images/logo.png" alt="Logo" height="75px"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
